ee55b15cf37b0f5e3ee07e9f338cbbd3c042a719
dhuck <dhuck@tinycact.us>
2024-03-18 09:37:09 -0500

    black formatting

--- yacgo/algos/mcts.py
+++ yacgo/algos/mcts.py
@@ -3,6 +3,7 @@ from typing import List
 
 from yacgo.go import game, govars
 
+
 class MCTSSearch:
     def __init__(self, state, model, root=None, noise=False, c_puct=1.1, komi=0):
         self.state = state
@@ -22,7 +23,7 @@ class MCTSSearch:
             self.root.parent = None
             if not root.initialized:
                 root.initialize()
-        
+
     def sim(self):
         child = self.root.best_child(self.c_puct)
         child.parent.backup(-child.value)
@@ -35,15 +36,18 @@ class MCTSSearch:
     def best_action(self):
         if self.sims_run < self.root.valid_move_count:
             raise ValueError(f"# sims must be at least {self.root.valid_move_count}")
-        
+
         return np.argmax(self.action_probs())
-    
+
     def action_probs(self):
         if self.sims_run < self.root.valid_move_count:
             raise ValueError(f"# sims must be at least {self.root.valid_move_count}")
-        
-        scores = [c.total_visits / self.root.total_visits if c is not None else -np.inf for c in self.root.children]
-        
+
+        scores = [
+            c.total_visits / self.root.total_visits if c is not None else -np.inf
+            for c in self.root.children
+        ]
+
         e = np.exp(scores)
         return e / np.sum(e)
 
@@ -67,35 +71,72 @@ class MCTSNode:
         self.initialized = True
         self.total_visits += 1
         if game.game_ended(self.state):
-            self.value = game.winning(self.state, self.search.komi) * game.turn_pm(self.state)
+            self.value = game.winning(self.state, self.search.komi) * game.turn_pm(
+                self.state
+            )
             self.terminal = True
         else:
-            self.value, self.policy = self.search.model.forward(self.state) # TODO: Can we remove invalid moves from channel features?
+            self.value, self.policy = self.search.model.forward(
+                self.state
+            )  # TODO: Can we remove invalid moves from channel features?
             self.terminal = False
-            self.children = [(MCTSNode(game.next_state(self.state, a), parent=self, search=self.search)) if self.valid_moves[a] == 1 else None for a in range(game.action_size(self.state))]
+            self.children = [
+                (
+                    (
+                        MCTSNode(
+                            game.next_state(self.state, a),
+                            parent=self,
+                            search=self.search,
+                        )
+                    )
+                    if self.valid_moves[a] == 1
+                    else None
+                )
+                for a in range(game.action_size(self.state))
+            ]
 
     def value_score(self):
         if self.value == 0:
             return 0
         return self.value / self.total_visits
-    
+
     def noisy_policy(self):
         if not self.search.noise:
             return self.policy
         else:
-            return 0.75 * self.policy \
-                + 0.25 * np.random.dirichlet([0.03 * game.action_size(self.state) / (c.valid_move_count + 1) if c is not None else np.nextafter(0, 1) for c in self.children])
+            return 0.75 * self.policy + 0.25 * np.random.dirichlet(
+                [
+                    (
+                        0.03 * game.action_size(self.state) / (c.valid_move_count + 1)
+                        if c is not None
+                        else np.nextafter(0, 1)
+                    )
+                    for c in self.children
+                ]
+            )
 
     def best_child(self, c_puct=1.1):
-        if self.terminal: # TODO: we could also return None to prevent an extra backprop if we want
+        if (
+            self.terminal
+        ):  # TODO: we could also return None to prevent an extra backprop if we want
             self.total_visits += 1
             return self
-            
+
         # TODO: refactor for clarity
         noisy_policy = self.noisy_policy()
 
-        puct = [-c.value_score() + c_puct * noisy_policy[i] * np.sqrt(self.total_visits - 1) / (1 + self.children[i].total_visits) 
-            if self.valid_moves[i] == 1 else -np.inf for i, c in enumerate(self.children)]
+        puct = [
+            (
+                -c.value_score()
+                + c_puct
+                * noisy_policy[i]
+                * np.sqrt(self.total_visits - 1)
+                / (1 + self.children[i].total_visits)
+                if self.valid_moves[i] == 1
+                else -np.inf
+            )
+            for i, c in enumerate(self.children)
+        ]
 
         # print(puct)
         child_to_expand = np.argmax(puct)
