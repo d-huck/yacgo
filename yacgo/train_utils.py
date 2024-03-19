f4471d43b921643ecebea5c853a91eb49762bcaa
dhuck <dhuck@tinycact.us>
2024-03-19 12:02:03 -0500

    catch KeyboardInterrupt

--- yacgo/train_utils.py
+++ yacgo/train_utils.py
@@ -1,5 +1,6 @@
 """
 Utils for training and evaluating models. 
+TODO: Go through and ensure all numpy is yacgo.data.DATA_DTYPE
 """
 
 # pylint: disable=missing-function-docstring,missing-class-docstring
@@ -13,7 +14,7 @@ from yacgo.algos.mcts import MCTSSearch
 from yacgo.data import TrainState
 from yacgo.game import Game
 from yacgo.go import game, govars
-from yacgo.data import DataGameClientMixin
+from yacgo.data import DataGameClientMixin, DATA_DTYPE
 from yacgo.player import MCTSPlayer, RandomPlayer
 from yacgo.models import Model
 
@@ -25,6 +26,7 @@ class GameGenerator(DataGameClientMixin):
         args: dict,
     ):
         super().__init__(args)
+
         self.board_size = args.board_size
         self.model = model
         self.komi = args.komi
@@ -34,28 +36,34 @@ class GameGenerator(DataGameClientMixin):
         self.args = args
 
     def sim_game(self):
-        data: List[TrainState] = []
-        state = game.init_state(self.board_size)
-        mcts = MCTSSearch(state, self.model, self.args, noise=True, root=None)
-        while not game.game_ended(state):
-            train = np.random.random() < self.pcap_prob
-            mcts.run_sims(self.pcap_train if train else self.pcap_fast)
-            action_probs, nodes = mcts.action_probs_nodes()
-            if train:
-                data.append(TrainState(state, 0, action_probs))
+        try:
+            data: List[TrainState] = []
+            state = game.init_state(self.board_size)
+            mcts = MCTSSearch(state, self.model, self.args, noise=True, root=None)
+            while not game.game_ended(state):
+                train = np.random.random() < self.pcap_prob
+                mcts.run_sims(self.pcap_train if train else self.pcap_fast)
+                action_probs, nodes = mcts.action_probs_nodes()
+                if train:
+                    data.append(TrainState(state, np.float32(0.0), action_probs))
 
-            action = np.random.choice(
-                np.arange(game.action_size(state)), p=action_probs
-            )
-            state = game.next_state(state, action)
-            mcts = MCTSSearch(
-                state, self.model, self.args, root=nodes[action], noise=True
-            )
+                action = np.random.choice(
+                    np.arange(game.action_size(state)), p=action_probs
+                )
+                state = game.next_state(state, action)
+                mcts = MCTSSearch(
+                    state, self.model, self.args, root=nodes[action], noise=True
+                )
 
-        winner = game.winning(state)
-        for d in data:
-            d.value = winner * game.turn_pm(d.state)
-            self.deposit(d)
+            winner = game.winning(state)
+            for d in data:
+                # TODO: ensure DATA_DTYPE all the way through
+                d.value = DATA_DTYPE(winner * game.turn_pm(d.state))
+                self.deposit(d)
+
+        except KeyboardInterrupt:
+            print("Quitting game generation, closing sockets...")
+            self.destroy()
 
         return data
 
