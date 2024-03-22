08d02f923d6d9f31d1176dc532a0c3adcd924c31
dhuck <dhuck@tinycact.us>
2024-03-22 17:36:00 -0500

    multi-threaded competition

--- yacgo/eval.py
+++ yacgo/eval.py
@@ -3,19 +3,27 @@ Code for evaluating networks.
 """
 
 from dataclasses import dataclass
-from typing import List, Tuple
+import multiprocessing as mp
+from multiprocessing import Pool, Lock
+from typing import Tuple
 
 import numpy as np
 from tqdm.auto import tqdm
 
 from yacgo.game import Game
-from yacgo.go import game, govars
-from yacgo.models import Model
+from yacgo.go import govars
+from yacgo.models import Model, InferenceClient
 from yacgo.player import MCTSPlayer, RandomPlayer
+
+# mp.set_start_method("fork")
+BW_GAME = 0
+WB_GAME = 1
 
 
 @dataclass
 class CompetitionResult:
+    """Dataclass for holding competition results."""
+
     score: int  # sum of results (+ means model1 performed better)
     probs: Tuple[int]  # (m1 win%, m2 win%)
     games: np.ndarray  # [-1, 1, 1, -1, 0, ...] (+ means model1 win)
@@ -25,61 +33,88 @@ class CompetitionResult:
 
 
 class ModelCompetition:
-    def __init__(self, board_size, model1, model2, args):
-        self.board_size = board_size
+    """
+    Class for handling model competitions between two models.
+    If either model is None, a random player will be used.
+    """
 
+    def __init__(self, model1: int, model2: int, args, n_workers=16):
+        self.board_size = args.board_size
         self.model1 = model1
         self.model2 = model2
         self.sims = args.n_simulations
         self.komi = args.komi
         self.args = args
         self.pbar = tqdm(total=1, unit="game", postfix={"score": 0.0})
+        self.lock = Lock()
+        self.n_workers = n_workers
+        self.scores = []
+
+    @staticmethod
+    def play_game(game_args):
+        """Plays an individual game in the competition.
+
+        Args:
+            game_num (which game number is this): keeps track of which games are ongoing
+            players (str): "bw" or "wb" to determine who plays black and white
+
+        Returns:
+            Result: winner of the game 1 for black, -1 for white, 0 for draw
+        """
+        model1, model2, args, komi = game_args
+        if model1 is None:
+            p1 = RandomPlayer(govars.BLACK)
+        else:
+            model = InferenceClient([model1])
+            p1 = MCTSPlayer(govars.BLACK, model, args)
+        if model2 is None:
+            p2 = RandomPlayer(govars.WHITE)
+        else:
+            model = InferenceClient([model2])
+            p2 = MCTSPlayer(govars.WHITE, model, args)
+
+        g = Game(args.board_size, p1, p2, komi)
+
+        result = g.play_full()
+        return result
 
     def compete(self, num_games=1) -> CompetitionResult:
+        """Run competition between two models.
+
+        Args:
+            num_games (int, optional): Number of games in competition. Defaults to 1.
+
+        Returns:
+            CompetitionResult: results of the competition
+        """
         bw_games = num_games // 2
         wb_games = num_games - bw_games
-        scores = []
-        self.pbar.reset(total=num_games)
 
-        def avg_score():
-            return f"{sum(scores) / len(scores):04.4f}"
+        self.pbar.reset(total=num_games)
 
         raw_bw_results = []
         self.pbar.set_description("Black vs White")
-        for _ in range(bw_games):
-            if self.model1 is None:
-                b_player = RandomPlayer(govars.BLACK)
-            else:
-                b_player = MCTSPlayer(govars.BLACK, self.model1, self.args)
-            if self.model2 is None:
-                w_player = RandomPlayer(govars.WHITE)
-            else:
-                w_player = MCTSPlayer(govars.WHITE, self.model2, self.args)
-
-            g = Game(self.board_size, b_player, w_player, self.komi)
-            result = g.play_full()
-            scores.append(result)
-            self.pbar.set_postfix({"score": sum(scores)})
-            self.pbar.update(1)
-            raw_bw_results.append(result)
+        bw_args = [
+            (self.model1, self.model2, self.args, self.komi) for _ in range(bw_games)
+        ]
+        with Pool(self.n_workers) as p:
+            for result in p.imap(self.play_game, bw_args):
+                raw_bw_results.append(result)
+                self.scores.append(result)
+                self.pbar.update(1)
+                self.pbar.set_postfix({"score": sum(self.scores)})
 
         raw_wb_results = []
+        wb_args = [
+            (self.model2, self.model1, self.args, self.komi) for _ in range(wb_games)
+        ]
         self.pbar.set_description("White vs Black")
-        for _ in range(wb_games):
-            if self.model2 is None:
-                b_player = RandomPlayer(govars.BLACK)
-            else:
-                b_player = MCTSPlayer(govars.BLACK, self.model2, self.args)
-            if self.model1 is None:
-                w_player = RandomPlayer(govars.WHITE)
-            else:
-                w_player = MCTSPlayer(govars.WHITE, self.model1, self.args)
-            g = Game(self.board_size, b_player, w_player, self.komi)
-            result = g.play_full()
-            scores.append(result)
-            self.pbar.set_postfix({"score": sum(scores)})
-            self.pbar.update(1)
-            raw_wb_results.append(result)
+        with Pool(self.n_workers) as p:
+            for result in p.imap_unordered(self.play_game, wb_args):
+                raw_wb_results.append(result)
+                self.scores.append(result * -1)
+                self.pbar.update(1)
+                self.pbar.set_postfix({"score": sum(self.scores)})
 
         raw_bw_results = np.array(raw_bw_results)
         raw_wb_results = np.array(raw_wb_results)
