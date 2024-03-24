62207a9f76a5d981b4656d9d4613a036a692698c
dhuck <dhuck@tinycact.us>
2024-03-23 21:20:39 -0500

    initial stack code

--- yacgo/scripts/stack.py
+++ yacgo/scripts/stack.py
@@ -2,12 +2,17 @@
 Runs the entire yacgo stack: 1 Trainer, 1 DataBroker, n InferenceServers, k Gameplay clients.
 """
 
+import os
 from multiprocessing import Process
 
 from yacgo.data import DataBroker, DataGameClientMixin
+from yacgo.eval import ModelCompetition
 from yacgo.models import InferenceClient, InferenceServer, Trainer
 from yacgo.train_utils import GameGenerator
 from yacgo.utils import make_args
+
+OLD_MODEL_PORT = 31337
+NEW_MODEL_PORT = 31338
 
 
 def inference_worker(port, args):
@@ -18,7 +23,6 @@ def inference_worker(port, args):
         args (dict): args dict.
     """
     server = InferenceServer(port, args)
-    print("Starting server...")
     server.run()
 
 
@@ -30,7 +34,6 @@ def databroker_worker(args):
         args (dict): args dict.
     """
     broker = DataBroker(args)
-    print("Starting databroker...")
     broker.run()
 
 
@@ -40,9 +43,82 @@ def trainer_worker(args):
     Args:
         args (dict): args dict.
     """
+    databroker = Process(target=databroker_worker, args=(args,), daemon=True)
+    databroker.start()
     trainer = Trainer(args)
-    print("Starting trainer...")
-    trainer.run()
+    old_model = args.model_path
+    iter = args.epoch
+    print("Starting training loop...")
+    while True:
+        servers = []
+        games = []
+        ports = list(range(args.inference_server_port, args.num_servers))
+        for port in ports:
+            servers.append(
+                Process(
+                    target=inference_worker, args=(port, args, old_model), daemon=True
+                )
+            )
+
+        for server in servers:
+            server.start()
+
+        for _ in range(args.num_games):
+            games.append(
+                Process(target=gameplay_worker, args=(ports, args), daemon=True)
+            )
+        for game in games:
+            game.start()
+
+        # run a training run and save the model
+        trainer.run()
+        model = trainer.save_pretrained(iter=iter)
+
+        # shutdown infererers
+        for server in servers:
+            server.terminate()
+            server.join()
+
+        for game in games:
+            game.terminate()
+            game.join()
+
+        # run the competition
+        if old_model is not None:
+            servers = [
+                Process(
+                    target=inference_worker,
+                    args=(OLD_MODEL_PORT, args, old_model),
+                    daemon=True,
+                ),
+                Process(
+                    target=inference_worker,
+                    args=(NEW_MODEL_PORT, args, model),
+                    daemon=True,
+                ),
+            ]
+        else:
+            servers = [
+                Process(
+                    target=inference_worker,
+                    args=(NEW_MODEL_PORT, args, model),
+                    daemon=True,
+                ),
+            ]
+        for server in servers:
+            server.start()
+        comp = ModelCompetition(NEW_MODEL_PORT, OLD_MODEL_PORT, args, n_workers=16)
+        result = comp.compete(num_games=400)
+        if result.probs[0] >= 0.55:
+            old_model = model
+            trainer.reset_data()
+            iter += 1
+        else:
+            trainer.load_pretrained(old_model)
+            os.remove(model)
+        for server in servers:
+            server.terminate()
+            server.join()
 
 
 def gameplay_worker(ports, args):
@@ -75,41 +151,16 @@ def main():
     """
     args = make_args()
     try:
-        servers = []
-        ports = list(range(args.inference_server_port, args.num_servers))
-        for port in ports:
-            servers.append(
-                Process(target=inference_worker, args=(port, args), daemon=True)
-            )
-
-        for server in servers:
-            server.start()
-
-        # Start the databroker
-        broker = Process(target=databroker_worker, args=(args,), daemon=True)
-        broker.start()
-
         # Start the trainer
         trainer = Process(target=trainer_worker, args=(args,))
         trainer.start()
-
-        # start the games
-        games = []
-        for _ in range(args.num_games):
-            games.append(
-                Process(target=gameplay_worker, args=(ports, args), daemon=True)
-            )
-        for g in games:
-            g.start()
-
-        for s in servers:
-            s.join()
-        broker.join()
         trainer.join()
-        for g in games:
-            g.join()
 
     except KeyboardInterrupt:
         pass
     finally:
         print("Exiting...")
+
+
+if __name__ == "__main__":
+    main()
