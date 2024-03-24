7c08d5b0ce9a9cf68b424104824c1d427fde9692
dhuck <dhuck@tinycact.us>
2024-03-24 11:49:33 -0500

    try block to catch keyboardinterrupt

--- yacgo/scripts/stack.py
+++ yacgo/scripts/stack.py
@@ -72,89 +72,108 @@ def trainer_worker(args):
     iter = args.epoch
     print("Starting training loop...")
     while True:
-        servers = []
-        games = []
-        ports = list(
-            range(
-                args.inference_server_port,
-                args.inference_server_port + args.num_servers,
-            )
-        )
-        print("Ports", ports)
-        for port in ports:
-            servers.append(
-                Process(
-                    target=inference_worker, args=(port, args, old_model), daemon=True
+        try:
+            model = None
+            servers = []
+            games = []
+            ports = list(
+                range(
+                    args.inference_server_port,
+                    args.inference_server_port + args.num_servers,
                 )
             )
-
-        for server in servers:
-            server.start()
-        display = True
-        for i in range(args.num_games):
-            games.append(
-                Process(
-                    target=gameplay_worker, args=(ports, i, display, args), daemon=True
+            print("Ports", ports)
+            for port in ports:
+                servers.append(
+                    Process(
+                        target=inference_worker,
+                        args=(port, args, old_model),
+                        daemon=True,
+                    )
                 )
+
+            for server in servers:
+                server.start()
+            display = True
+            for i in range(args.num_games):
+                games.append(
+                    Process(
+                        target=gameplay_worker,
+                        args=(ports, i, display, args),
+                        daemon=True,
+                    )
+                )
+                display &= False
+            for i, game in enumerate(games):
+                game.start()
+                if i % 16 == 15:  # slow spin up of the games
+                    time.sleep(5)
+
+            # run a training run and save the model
+            trainer.run()
+            model = trainer.save_pretrained(iter=iter)
+
+            # shutdown infererers
+            for server in servers:
+                server.terminate()
+                server.join()
+
+            for game in games:
+                game.terminate()
+                game.join()
+
+            # run the competition
+            if old_model is not None:
+                servers = [
+                    Process(
+                        target=inference_worker,
+                        args=(OLD_MODEL_PORT, args, old_model),
+                        daemon=True,
+                    ),
+                    Process(
+                        target=inference_worker,
+                        args=(NEW_MODEL_PORT, args, model),
+                        daemon=True,
+                    ),
+                ]
+            else:
+                servers = [
+                    Process(
+                        target=inference_worker,
+                        args=(NEW_MODEL_PORT, args, model),
+                        daemon=True,
+                    ),
+                ]
+            for server in servers:
+                server.start()
+            comp = ModelCompetition(
+                NEW_MODEL_PORT, OLD_MODEL_PORT, args, n_workers=args.num_games
             )
-            display &= False
-        for i, game in enumerate(games):
-            game.start()
-            if i % 16 == 15:  # slow spin up of the games
-                time.sleep(5)
-
-        # run a training run and save the model
-        trainer.run()
-        model = trainer.save_pretrained(iter=iter)
-
-        # shutdown infererers
-        for server in servers:
-            server.terminate()
-            server.join()
-
-        for game in games:
-            game.terminate()
-            game.join()
-
-        # run the competition
-        if old_model is not None:
-            servers = [
-                Process(
-                    target=inference_worker,
-                    args=(OLD_MODEL_PORT, args, old_model),
-                    daemon=True,
-                ),
-                Process(
-                    target=inference_worker,
-                    args=(NEW_MODEL_PORT, args, model),
-                    daemon=True,
-                ),
-            ]
-        else:
-            servers = [
-                Process(
-                    target=inference_worker,
-                    args=(NEW_MODEL_PORT, args, model),
-                    daemon=True,
-                ),
-            ]
-        for server in servers:
-            server.start()
-        comp = ModelCompetition(
-            NEW_MODEL_PORT, OLD_MODEL_PORT, args, n_workers=args.num_games
-        )
-        result = comp.compete(num_games=args.num_comp_games)
-        if result.probs[0] >= 0.55:
-            old_model = model
-            trainer.reset_data()
-            iter += 1
-        else:
-            trainer.load_pretrained(old_model)
-            os.remove(model)
-        for server in servers:
-            server.terminate()
-            server.join()
-        del comp
+            result = comp.compete(num_games=args.num_comp_games)
+            if result.probs[0] >= 0.55:
+                old_model = model
+                trainer.reset_data()
+                iter += 1
+            else:
+                trainer.load_pretrained(old_model)
+                os.remove(model)
+            for server in servers:
+                server.terminate()
+                server.join()
+            del comp
+        except KeyboardInterrupt:
+            print("Quitting training, closing sockets...")
+            for server in servers:
+                if server is not None:
+                    server.terminate()
+                    server.join()
+            for game in games:
+                if game is not None:
+                    game.terminate()
+                    game.join()
+            if model is not None:
+                os.remove(model)  # don't store a bad model
+            break
 
 
 def main():
