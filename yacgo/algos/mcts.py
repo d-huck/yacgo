random symmetry and softmax temp fixes

yacgo/algos/mcts.py
@@ -90,7 +90,7 @@ class MCTSNode:
             )  # TODO: Can we remove invalid moves from channel features?
             self.value = self.value * game.turn_pm(self.state)
             self.policy *= self.valid_moves
-            e_x = np.exp(self.policy / (self.search.args.softmax_temp if self == self.search.root and self.search.noise else 1))
+            e_x = np.exp(self.policy / (self.search.softmax_temp if self == self.search.root and self.search.noise else 1))
             self.policy = e_x / np.sum(e_x)
             self.terminal = False
 

yacgo/data.py
@@ -266,16 +266,17 @@ class DataBroker(object):
         pol_pass = policy[-1]
         policy_1d = policy[:-1]
         policy_2d = np.reshape(policy_1d, (state.shape[1], state.shape[1]))
+        print(policy_2d)
 
         orientation = np.random.randint(0, 8)
         if (orientation >> 0) % 2:
             # Horizontal flip
             state = np.flip(state, 2)
-            policy_2d = np.flip(policy_2d, 2)
+            policy_2d = np.flip(policy_2d, 1)
         if (orientation >> 1) % 2:
             # Vertical flip
             state = np.flip(state, 1)
-            policy_2d = np.flip(policy_2d, 1)
+            policy_2d = np.flip(policy_2d, 0)
         if (orientation >> 2) % 2:
             # Rotate 90 degrees
             state = np.rot90(state, axes=(1, 2))
@@ -312,6 +313,7 @@ class DataBroker(object):
                 state, value, policy = DataBroker.random_symmetry(data.state, data.value, data.policy)
             else:
                 state, value, policy = data.state, data.value, data.policy
+
             states.append(state)
             values.append(value)
             policies.append(policy)