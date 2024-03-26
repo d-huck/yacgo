e4ebdbddaf98be878d307082fc205995d75e9d2a
dhuck <dhuck@tinycact.us>
2024-03-25 22:33:53 -0500

    more reliable test using generated game

--- yacgo/tests/test_databroker.py
+++ yacgo/tests/test_databroker.py
@@ -2,9 +2,11 @@
 Simple script to test databroker flow
 """
 
+import multiprocessing as mp
 from multiprocessing import Process
-from yacgo.models import Trainer
+from yacgo.models import Trainer, InferenceRandom
 from yacgo.data import DataBroker, KataGoDataClient
+from yacgo.train_utils import GameGenerator
 from yacgo.utils import make_args
 
 
@@ -35,14 +37,20 @@ def main():
     """
     Main Process
     """
+    mp.set_start_method("spawn")
     args = make_args()
     try:
         databroker = Process(target=databroker_worker, args=(args,), daemon=True)
         databroker.start()
         trainer = Process(target=trainer_worker, args=(args,), daemon=True)
         trainer.start()
-        data = KataGoDataClient(args)
-        data.run("data/kata1-b40c256x2-s5095420928-d1229425124")
+        model = InferenceRandom()
+        game_gen = GameGenerator(model, args, display=True)
+        for _ in range(1024):
+            data = game_gen.sim_game()
+            print(data)
+        # data = KataGoDataClient(args)
+        # data.run("data/kata1-b40c256x2-s5095420928-d1229425124")
 
         trainer.join()
         databroker.join()
