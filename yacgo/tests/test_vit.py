4ad696525cd716d02076a5ca749c3aee6b819d98
dhuck <dhuck@tinycact.us>
2024-04-02 12:53:48 -0500

    test eval and train separate

--- yacgo/tests/test_vit.py
+++ yacgo/tests/test_vit.py
@@ -12,16 +12,16 @@ def main():
     example = torch.randn(
         1024, args.num_feature_channels, args.board_size, args.board_size
     ).to(args.device)
-    try:
-        trainer.model.eval()
-        print("testing eval")
-        trainer.model.forward(example)
-        trainer.model.train()
-        trainer.model.forward(example)
-        print("testing train")
-    except RuntimeError as e:
-        print(e)
-        print("Model not implemented yet.")
+    # try:
+    trainer.model.eval()
+    print("testing eval")
+    trainer.model.forward(example)
+    trainer.model.train()
+    trainer.model.forward(example)
+    print("testing train")
+    # except RuntimeError as e:
+    #     print(e)
+    #     print("Model not implemented yet.")
 
     print("Success!")
 
