2834c2dbbbca7b4ba251b181b3ab53aa2d7390b2
dhuck <dhuck@tinycact.us>
2024-04-28 15:03:53 -0500

    install requirements with setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,17 @@
 from setuptools import setup, find_packages
+import os
+
+
+lib_folder = os.path.dirname(os.path.realpath(__file__))
+req_path = os.path.join(lib_folder, "requirements.txt")
+install_requires = []
+if os.path.exists(req_path):
+    with open(req_path, "r") as f:
+        install_requires = f.read().splitlines()
 
 setup(
     name="yacgo",
     version="0.1",
     packages=find_packages(),
+    install_requires=install_requires,
 )
